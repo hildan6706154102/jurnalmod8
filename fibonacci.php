1c73b065c8f101d78d928ec493c2bda95aafe6b8
Hildan Fawwaz Naufal <hildanfn01@gmail.com>
2017-04-07 09:53:32 +0700

    Add files via upload

---
+++ fibonacci.php
@@ -0,0 +1,29 @@
+<?php
+
+function printFibonacci($n)
+ {
+ 
+  $first = 1;
+  $second = 1;
+ 
+  echo "Fibonacci Series \n";
+ 
+  echo $first.' '.$second.' ';
+ 
+  for($i = 2; $i < $n; $i++){
+ 
+    $third = $first + $second;
+ 
+    echo $third.' ';
+ 
+    $first = $second;
+    $second = $third;
+ 
+    }
+}
+  
+/* Function call to print Fibonacci series upto 4 numbers. */
+ 
+printFibonacci(7);
+
+?>
